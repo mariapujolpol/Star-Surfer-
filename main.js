5b704d842165168e5fec8825beaefac6db541d53
Maria Pol <maria.pujolpol@gmail.com>
2026-01-26 19:41:31 +0100

    Add more logic

---
+++ main.js
@@ -0,0 +1,15 @@
+/*DOM ELEMENTS*/
+
+const startScreen = document.querySelector('#start-screen');
+const gameScreen = document.querySelector('#game-screen');
+const gameOverScreen = document.querySelector('#game-over-screen');
+const victoryScreen = document.querySelector('#victory-screen');
+
+const startBtn = document.querySelector('#start-btn');
+
+const gameBox = document.querySelector('#game-box');
+const player = document.querySelector('#player');
+
+const scoreValue = document.querySelector('#score-value');
+const timerValue = document.querySelector('#timer-value');
+
